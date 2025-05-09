b32f6ae659ad169d83c45cff77f0cf5ed93bfb12
KyuHyung Lee <kyuhunglee01@gmail.com>
2025-05-09 16:06:12 +0900

    실습10주차

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -6,8 +6,12 @@ import uuid
 import json
 import os
 from filelock import FileLock
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+
+# Prometheus 메트릭스 엔드포인트 (/metrics)
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
 # To-Do 항목 모델
 class TodoItem(BaseModel):
